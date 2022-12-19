7f9a1939658a4043219433f8d37da36c48d20320
Zakiaatot <62224034+Zakiaatot@users.noreply.github.com>
2022-12-19 16:03:05 +0800

    v1.0.1

--- src/wechat.js
+++ src/wechat.js
@@ -2,27 +2,37 @@
 import { WechatyBuilder } from 'wechaty'
 import config from '../config.js'
 import { reply } from './openai.js'
+import qrcodeTerminal from 'qrcode-terminal'
 
 const wechaty = WechatyBuilder.build() // get a Wechaty instance
 
 
 const message_handler = async (message) => {
-    const room = message.room()
-    if (room && !message.self() && message.text().includes(config.BOT_NAME)) {
-        const topic = await room.topic()
-        if (config.WHITE_LIST.includes(topic) && message.type() === wechaty.Message.Type.Text) {
-            const alias = (await message.talker().alias()) || (await message.talker().name())
-            const text = message.text().replace(config.BOT_NAME, "")
-            const res = await reply(text)
-            room.say("@" + alias + " \n" + res)
+    try {
+        const room = message.room()
+        if (room && !message.self() && message.text().includes(config.BOT_NAME)) {
+            const topic = await room.topic()
+            if (config.WHITE_LIST.includes(topic) && message.type() === wechaty.Message.Type.Text) {
+                const alias = (await message.talker().alias()) || (await message.talker().name())
+                const text = message.text().replace(config.BOT_NAME, "")
+                const res = await reply(text)
+                await room.say("@" + alias + " \n" + res)
+            }
         }
+    } catch (error) {
+        await room.say("Error:", error)
     }
 
 }
 
 
+function onScan(qrcode) {
+    qrcodeTerminal.generate(qrcode); // 在console端显示二维码
+}
+
 wechaty
-    .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`))
+    .on('scan', onScan)
     .on('login', user => console.log(`User ${user} logged in`))
     .on('message', message_handler)
-wechaty.start()
+
+wechaty.start().catch((e) => console.error(e))
